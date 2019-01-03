953df1ce43f63297b7248bbc83d41bd46347499b
13068240601 <13068240601@163.com>
2019-01-03 08:24:27 +0800

    尝试获取用户权限

--- pages/index/index.js
+++ pages/index/index.js
@@ -386,21 +386,26 @@ Page({
   },
   onLoad: function (options) {
     var that = this
-    
-    // wx.getSetting({
-    //   success(res) {
-    //     console.log(res.authSetting['scope.userLocation'])
-    //     if (res.authSetting['scope.userLocation']==false){
-    //       console.log(123)
-    //       wx.authorize({
-    //         scope: 'scope.userLocation',
-    //         success() {
-    //           wx.startRecord()
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userLocation']) {
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success() {
+              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+              wx.getLocation({
+                type: 'wgs84',
+                success: function (res) {
+                  console.log(res)
+                }
+              })
+            }
+          })
+        }else{
+          console.log('   ')
+        }
+      }
+    })
     if(options.lng){
       that.setData({
         lng: options.lng,
